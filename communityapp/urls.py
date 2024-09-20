743841eddaf666269b943cda2f1414a15a2e4dc4
community-vercel <zuhooruddin@live.com>
2024-09-20 05:17:39 -0700

    commit

--- communityapp/urls.py
+++ communityapp/urls.py
@@ -30,21 +30,33 @@ urlpatterns = [
     path('update-business-status/', views.update_business_status, name='update-business-status'),
     path('get-category-businesses/', views.get_businesses_by_category, name='get-category-businesses'),
     path('fetch-more-businesses/', views.fetch_more_businesses_by_category, name='fetch-more-businesses'),
+    path('delete-businessimage/', views.delete_businessimage, name='delete_image'),
+    path('update-businessdata/',views.update_businessdata, name='update-business'),
+
 
 #Reviews    
     
     path('create-review/', views.create_review, name='create-review'),
     path('get-reviews/', views.get_reviews, name='get_reviews_for_business'),
     path('update-reviews/', views.update_reviews, name='update_reviews'),
+    path('get-specificreview/', views.get_specific_review, name='get_specific_reciews'),
 
     path('get-business-rating-stats/', views.get_business_rating_stats, name='get-business-rating-stats'),
     path('toggle-favorite/', views.toggle_favorite, name='toggle_favorite'),
     path('check-toggle-favorite/', views.check_and_toggle_favorite, name='check_toggle_favorite'),
     path('search-businesses/', views.search_businesses, name='search-businesses/'),
 
+  
+
+#User
+
+    path('get-users/', views.FetchUserDetailsView, name='get_users'),
+    path('add-user/', views.add_user, name='add_user'),
+    path('update-user/', views.update_user, name='update_user'),
     path('get-all-users/', views.get_all_users, name='get_all_users'),
     path('fetch-favorites/', views.fetch_favorites_for_user, name='fetch_favorites_for_user'),
-
+    path('delete-review-image/', views.delete_review_image, name='delete-review-image'),
+    path('update-reviewdata/',views.update_review_data, name='update-review-image'),
 
     #  path("google/", GoogleLoginView.as_view(), name = "google"),
     # path('api/auth/password/reset/confirm/<str:uidb64>/<str:token>', PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
